652035a641dc65c486d879b0ad4b6be4fa3df5c8
Natasha Nduku <natashanduku0@gmail.com>
2024-12-03 12:25:17 +0300

    Superbowl find function

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+const record = [
+    { year: '2015', result: 'W'},
+    { year: '2014', result: 'N/A'},
+    { year: '2015', result: 'L'},
+]
+const superbowlWin = (records) =>{
+    const win = records.find(record => record.result === 'W');
+    return win ? win.year : undefined;
+}
